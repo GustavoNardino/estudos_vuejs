e867f454b752911cf253aae2dce7e1e15d1f0030
gustavo <fgustavo.n@gmail.com>
2024-01-16 13:14:07 -0300

    store

--- src/views/CardsView.vue
+++ src/views/CardsView.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 import Card from '../components/Card.vue'
 import { getYugiCards, getYugiCardsList } from '@/services/cardsServices.ts'
+import { useCounterStore } from '@/stores/counter';
+
+const store = useCounterStore();
+store.count++;
+store.increment(2);
 </script>
 
 <template>
   <div class="cards">
     <h1>Cards page</h1>
+    <div>
+      <h2>State do pinia: </h2>
+      <h4>{{ store.count }}</h4>
+      <h4>{{ store.doubleCount }}</h4>
+      <button @click="store.increment(1)">Clique</button>
+    </div>
     <Card :image="cardData" />
     <button @click="handleReq($event)">GENERATE</button>
     <div class="grid">
